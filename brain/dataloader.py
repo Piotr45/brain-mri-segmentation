2cb8204d5cc341e78f34cd51a24841b66d7e47ea
Piotr45 <baryczkowski.piotr@gmail.com>
2023-12-14 17:14:04 +0100

    refactor: simplified call method, added dataset info

--- brain/dataloader.py
+++ brain/dataloader.py
@@ -12,7 +12,8 @@ import numpy as np
 
 from PIL import Image
 
-from utils import display, normalize
+from utils.io import display
+from utils.nn import normalize
 
 
 DATASET_PATH = os.path.join(
@@ -28,17 +29,25 @@ class DatasetLoaderGen:
 
     Attributes:
         path: A string that contains path where dataset will be located.
-        downloand: A boolean indicating if we want to download dataset with kaggle API or not.
+        resize_shape: A tuple that defines output image shape, None if you want the original.
+        dataset_info: Dict with basic information about dataset e.g. number of samples, and path to them.
     """
 
-    def __init__(self, path: str = DATASET_PATH, download: bool = True) -> None:
+    def __init__(
+        self,
+        path: str = DATASET_PATH,
+        resize_shape: tuple | None = None,
+        download: bool = True,
+    ) -> None:
         """Initializes the instance of DatasetLoader.
 
         Args:
             path: Defines path to dataset.
-            download: Defines if we want to download dataset via kaggle API.
+            resize_shape: If you want to resize output image, pass shape e.g. (128, 128).
+            downloand: A boolean indicating if we want to download dataset with kaggle API or not.
         """
-        self.path = path
+        self.path: str = path
+        self.resize_shape: tuple | None = resize_shape
 
         if download:
             kaggle.api.authenticate()
@@ -50,11 +59,34 @@ class DatasetLoaderGen:
                 unzip=True,
             )
 
+        self.dataset_info = self.__get_dataset_info()
+
     def __call__(self) -> tuple[np.ndarray, np.ndarray]:
         """Walks through dataset dirs and returns data.
 
         Returns:
             Tuple of images, original image and mask.
+        """
+        source_dirs = self.dataset_info["source_dirs"]
+        for source_dir in source_dirs:
+            mask_source_files = glob.glob(os.path.join(source_dir, "*_mask.tif"))
+            for mask_source in mask_source_files:
+                # yield original image and mask
+                # TODO find prettier way to do this
+                image = np.array(Image.open(mask_source.replace("_mask", "")))
+                mask = np.array(Image.open(mask_source))
+                # resize the data
+                if self.resize_shape is not None:
+                    image, mask = self.__resize_data(image, mask)
+                # normalize the data
+                normalized_image, normalized_mask = normalize(image, mask)
+                yield normalized_image, np.expand_dims(normalized_mask, axis=2)
+
+    def __get_dataset_info(self) -> dict:
+        """Get information about dataset.
+
+        Returns:
+            Basic information about dataset: source directories, number of samples in dataset.
         """
         source_dirs = [
             source_dir
@@ -63,23 +95,29 @@ class DatasetLoaderGen:
             )
             if os.path.isdir(source_dir)
         ]
-        for source_dir in source_dirs:
-            mask_source_files = glob.glob(os.path.join(source_dir, "*_mask.tif"))
-            for mask_source in mask_source_files:
-                # yield original image and mask
-                # TODO find prettier way to do this
-                image = np.array(Image.open(mask_source.replace("_mask", "")))
-                mask = np.array(Image.open(mask_source))
-                resized_image = cv2.resize(
-                    image, (128, 128), interpolation=cv2.INTER_AREA
-                )
-                resized_mask = cv2.resize(
-                    mask, (128, 128), interpolation=cv2.INTER_AREA
-                )
-                normalized_image, normalized_mask = normalize(
-                    resized_image, resized_mask
-                )
-                yield normalized_image, np.expand_dims(normalized_mask, axis=2)
+        num_samples = sum(
+            [
+                len(glob.glob(os.path.join(source_dir, "*_mask.tif")))
+                for source_dir in source_dirs
+            ]
+        )
+        return {"source_dirs": source_dirs, "num_samples": num_samples}
+
+    def __resize_data(
+        self, image: np.ndarray, mask: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray]:
+        """Function for resizing data.
+
+        Args:
+            image: Image that we want to resize.
+            mask: Mask associated with the image.
+
+        Returns:
+            Resized data with resize_shape attribute.
+        """
+        return cv2.resize(
+            image, self.resize_shape, interpolation=cv2.INTER_AREA
+        ), cv2.resize(mask, self.resize_shape, interpolation=cv2.INTER_AREA)
 
 
 if __name__ == "__main__":
